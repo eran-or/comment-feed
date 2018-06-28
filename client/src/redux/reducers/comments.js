b64cef5b043b86e214dfa8edf079c8406584aae0
eranor <erano@welldone-software.com>
2018-06-28 11:30:23 +0300

    clean comments

--- client/src/redux/reducers/comments.js
+++ client/src/redux/reducers/comments.js
@@ -4,8 +4,6 @@ import update from 'immutability-helper'
 
 const defaultState = {}
 export default handleActions({
-  // [setComments]: (state, { payload: {comments} }) => Object.assign({},state,{comments}),
-  // [setActiveComment]: (state, { payload: {id} }) => Object.assign({},state,{lastActiveComment:id}),
   [setComments]: (state, { payload: {comments} }) => update(state,{comments:{$set:comments}}),
   [setActiveComment]: (state, { payload: {id} }) => update(state,{lastActiveComment:{$set:id}}),
 }, defaultState);
